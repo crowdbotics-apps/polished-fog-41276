f50177a8db1ee80f3efc8c6c25f9965ae8996494
Crowdbotics <team@crowdbotics.com>
2023-06-05 23:26:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "polished-fog-41276",
+  url: "https://polished-fog-41276.botics.co",
+  api: "https://polished-fog-41276.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
